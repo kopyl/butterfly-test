69ae768980cae94499492b7f286b33639acca658
kopyl <oleggggg1997@gmail.com>
2022-11-04 13:51:03 +0200

    i

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="imageContainer">
+        <div className="image">
+          <div className="left">
+            <svg
+              width="31"
+              height="47"
+              viewBox="0 0 31 47"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M2.08912 33.5593C0.7963 32.8123 0 31.4326 0 29.9395V4.18879C0 0.9698 3.48524 -1.04156 6.27238 0.568938L28.5493 13.4413C29.8421 14.1883 30.6384 15.568 30.6384 17.0611V42.8112C30.6384 46.0302 27.1532 48.0415 24.3661 46.4311L2.08912 33.5593Z"
+                fill="#BFBFBF"
+              />
+            </svg>
+          </div>
+          <div className="right">
+            <svg
+              width="31"
+              height="47"
+              viewBox="0 0 31 47"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M2.08912 33.5593C0.7963 32.8123 0 31.4326 0 29.9395V4.18879C0 0.9698 3.48524 -1.04156 6.27238 0.568938L28.5493 13.4413C29.8421 14.1883 30.6384 15.568 30.6384 17.0611V42.8112C30.6384 46.0302 27.1532 48.0415 24.3661 46.4311L2.08912 33.5593Z"
+                fill="#BFBFBF"
+              />
+            </svg>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
